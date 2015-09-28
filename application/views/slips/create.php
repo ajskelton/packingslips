48f156a4b074bc150aefe4b56647b0f8059b81a5
Anthony Skelton <anthony@redcartstudios.com>
2015-09-28 15:41:27 -0700

    Moved Device and Ship to into their own rows and added description field

--- application/views/slips/create.php
+++ application/views/slips/create.php
@@ -20,50 +20,52 @@ echo form_open('slips/create', $attributes);
 	</div>
 
 	<div class='row'>
+		<h2 class="center">Device</h2>
 		<div class='col-md-6'>
-			<h2 class='center'>Device</h2>
 			<div class='form-group'>
-				<label for='assetTag' class='col-sm-3 control-label'>Asset Tag Number</label>
-				<div class='col-sm-9'>
+				<label for='assetTag' class='col-sm-4 control-label'>Asset Tag Number</label>
+				<div class='col-sm-8'>
 					<input name="slip_assetTag" type='input' class='form-control' id='slip_assetTag' placeholder='' value="<?php echo set_value('slip_assetTag', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='manufacturer' class='col-sm-3 control-label'>Manufacturer</label>
-				<div class='col-sm-9'>
+				<label for='manufacturer' class='col-sm-4 control-label'>Manufacturer</label>
+				<div class='col-sm-8'>
 					<input name='slip_manufacturer' type='input' class='form-control' id='slip_manufacturer' placeholder='' value="<?php echo set_value('slip_manufacturer', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='deviceName' class='col-sm-3 control-label'>Device Name</label>
-				<div class='col-sm-9'>
+				<label for='deviceName' class='col-sm-4 control-label'>Device Name</label>
+				<div class='col-sm-8'>
 					<input name='slip_deviceName' type='input' class='form-control' id='slip_deviceName' placeholder='' value="<?php echo set_value('slip_deviceName', ''); ?>">
 				</div>
 			</div>
+		</div>
+		<div class="col-md-6">
 			<div class='form-group'>
-				<label for='modelNumber' class='col-sm-3 control-label'>Model Number</label>
-				<div class='col-sm-9'>
+				<label for='modelNumber' class='col-sm-4 control-label'>Model Number</label>
+				<div class='col-sm-8'>
 					<input name='slip_modelNumber' type='input' class='form-control' id='slip_modelNumber' placeholder='' value="<?php echo set_value('slip_modelNumber', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='serialNumber' class='col-sm-3 control-label'>Serial Number</label>
-				<div class='col-sm-9'>
+				<label for='serialNumber' class='col-sm-4 control-label'>Serial Number</label>
+				<div class='col-sm-8'>
 					<input name='slip_serialNumber' type='input' class='form-control' id='slip_serialNumber' placeholder='' value="<?php echo set_value('slip_serialNumber', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='slip_quantity' class='col-sm-3 control-label'>Quantity</label>
-				<div class='col-sm-9'>
+				<label for='slip_quantity' class='col-sm-4 control-label'>Quantity</label>
+				<div class='col-sm-8'>
 					<input name='slip_quantity' type='input' class='form-control' id='slip_quantity' placeholder='' value="<?php echo set_value('slip_quantity', ''); ?>">
 				</div>
 			</div>
 		</div>
+		<h2 class='center'>Ship To:</h2>
 		<div class='col-md-6'>
-			<h2 class='center'>Ship To:</h2>
 			<div class='form-group'>
-				<label for='presets' class='col-sm-3 control-label'>Presets</label>
-				<div class='col-sm-9'>
+				<label for='presets' class='col-sm-4 control-label'>Presets</label>
+				<div class='col-sm-8'>
 					<select class='select-address form-control' id='select-address'> <!-- This select pulls the names of vendors from the db and uses them as options. On selecting an option the address fields are filled in via javascript at the end of this file -->
 						<option value='none' selected='selected'>None</option>
 						<?php
@@ -77,32 +79,34 @@ echo form_open('slips/create', $attributes);
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='slip_shipName' class='col-sm-3 control-label'>Name</label>
-				<div class='col-sm-9'>
+				<label for='slip_shipName' class='col-sm-4 control-label'>Name</label>
+				<div class='col-sm-8'>
 					<input name='slip_shipName' type='input' class='form-control' id='slip_shipName' placeholder='' value="<?php echo set_value('slip_shipName', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='device-name' class='col-sm-3 control-label'>Address</label>
-				<div class='col-sm-9'>
+				<label for='device-name' class='col-sm-4 control-label'>Address</label>
+				<div class='col-sm-8'>
 					<input name='slip_shipAddress' type='input' class='form-control' id='slip_shipAddress' placeholder='' value="<?php echo set_value('slip_shipAddress', ''); ?>">
 				</div>
 			</div>
+		</div>
+		<div class="col-md-6">
 			<div class='form-group'>
-				<label for='device-name' class='col-sm-3 control-label'>City</label>
-				<div class='col-sm-9'>
+				<label for='device-name' class='col-sm-4 control-label'>City</label>
+				<div class='col-sm-8'>
 					<input name='slip_shipCity' type='input' class='form-control' id='slip_shipCity' placeholder='' value="<?php echo set_value('slip_shipCity', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='device-name' class='col-sm-3 control-label'>State</label>
-				<div class='col-sm-9'>
+				<label for='device-name' class='col-sm-4 control-label'>State</label>
+				<div class='col-sm-8'>
 					<input name='slip_shipState' type='input' class='form-control' id='slip_shipState' placeholder='' value="<?php echo set_value('slip_shipState', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='device-name' class='col-sm-3 control-label'>Zip</label>
-				<div class='col-sm-9'>
+				<label for='device-name' class='col-sm-4 control-label'>Zip</label>
+				<div class='col-sm-8'>
 					<input name='slip_shipZip' type='input' class='form-control' id='slip_shipZip' placeholder='' value="<?php echo set_value('slip_shipZip', ''); ?>">
 				</div>
 			</div>
@@ -112,40 +116,46 @@ echo form_open('slips/create', $attributes);
 		<h2 class='center'>Other Information</h2>
 		<div class='col-md-6'>
 			<div class='form-group'>
-				<label for='device-name' class='col-sm-3 control-label'>FedEx Tracking Number</label>
-				<div class='col-sm-9'>
+				<label for='device-name' class='col-sm-4 control-label'>FedEx Tracking Number</label>
+				<div class='col-sm-8'>
 					<input name='slip_fedexTracking' type='input' class='form-control' id='slip_fedexTracking' placeholder='' value="<?php echo set_value('slip_fedexTracking', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='device-name' class='col-sm-3 control-label'>RMA Number</label>
-				<div class='col-sm-9'>
+				<label for='device-name' class='col-sm-4 control-label'>RMA Number</label>
+				<div class='col-sm-8'>
 					<input name='slip_rmaNumber' type='input' class='form-control' id='rmaNumber' placeholder='' value="<?php echo set_value('slip_rmaNumber', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='slip_comments' class='col-sm-3 control-label'>Additional Comments</label>
-				<div class='col-sm-9'>
+				<label for='slip_comments' class='col-sm-4 control-label'>Additional Comments</label>
+				<div class='col-sm-8'>
 					<textarea name='slip_comments' type='textarea' class='form-control' id='slip_comments' rows='3' placeholder=''><?php echo set_value('slip_comments', ''); ?></textarea>
 				</div>
 			</div>
 		</div>
 		<div class="col-md-6">
 			<div class='form-group'>
-				<label for='slip_customerContact' class='col-sm-3 control-label'>Customer Contact</label>
-				<div class='col-sm-9'>
+				<label for='slip_customerContact' class='col-sm-4 control-label'>Customer Contact</label>
+				<div class='col-sm-8'>
 					<input name='slip_customerContact' type='input' class='form-control' id='slip_customerContact' placeholder='' value="<?php echo set_value('slip_customerContact', ''); ?>">
 				</div>
 			</div>
 			<div class='form-group'>
-				<label for='slip_customerPhone' class='col-sm-3 control-label'>Customer Phone #</label>
-				<div class='col-sm-9'>
+				<label for='slip_customerPhone' class='col-sm-4 control-label'>Customer Phone #</label>
+				<div class='col-sm-8'>
 					<input name='slip_customerPhone' type='input' class='form-control' id='slip_customerPhone' placeholder='' value="<?php echo set_value('slip_customerPhone', ''); ?>">
 				</div>
 			</div>
 			<div class="form-group">
-				<label for="status" class="col-sm-3 control-label">Status</label>
-				<div class="col-sm-9">
+				<label for="description" class="col-sm-4 control-label">Packing Slip Description</label>
+				<div class="col-sm-8">
+					<input name='slip_description' type="input" class='form-control' id='slip_description' placeholder='' value="<?php echo set_value('slip_description', ''); ?>">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="status" class="col-sm-4 control-label">Status</label>
+				<div class="col-sm-8">
 					<?php 
 					$status_options = array(
 						'Created' => 'Created',
@@ -158,6 +168,7 @@ echo form_open('slips/create', $attributes);
 					echo form_dropdown('slip_status', $status_options, set_value('slip_status'), $status_classes);
 					?>
 				</div>
+				
 			</div>
 		</div>
 	</div>
