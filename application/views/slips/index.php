ad813d3dfeb5ea4b4b60b79787dcebb49531a007
Anthony Skelton <anthony@redcartstudios.com>
2015-10-24 04:52:43 -0700

    Removed complete slips from index page

--- application/views/slips/index.php
+++ application/views/slips/index.php
@@ -37,28 +37,3 @@ foreach ($reverseSlips as $slips_item):
 endforeach ?>
 
 </table>
-<h2>Completed Slips</h2>
-<table class="table table-striped">
-	<tr>
-		<th>ID</th>
-		<th>Description</th>
-		<th>Shipped To</th>
-		<th>FedEx Tracking #</th>
-		<th>Date Created</th>
-		<th>Last Modified</th>
-		<th>Status</th>
-	</tr>
-	<?php
-	foreach ($reverseSlips as $slips_item) :
-		if ($slips_item['slip_status'] == 'Complete')
-		echo "<tr><td><a href='slips/" . $slips_item['slip_id'] . "'>" . $slips_item['slip_id'] .
-                            "</a></td><td><a href='slips/" . $slips_item['slip_id'] . "'>" . $slips_item['slip_description'] . 
-                            "</a></td><td>" . $slips_item['slip_shipName'] . 
-                            "</td><td><a target='_blank' href='https://www.fedex.com/apps/fedextrack/?action=track&language=english&cntry_code=us&tracknumbers=" . $slips_item['slip_fedexTracking'] . "'>" . $slips_item['slip_fedexTracking'] .
-                            "</td><td>" . date( 'F j, Y g:i a', strtotime($slips_item['slip_date'])) . 
-                            "</td><td>" . $slips_item['slip_lastModified'] .
-                            "</td><td>" . $slips_item['slip_status'] . 
-                            "</td><tr>"; 
-      endforeach ?>
-
-</table>
