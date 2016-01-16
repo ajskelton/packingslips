44639b7ebd101e09a8b7ef412a6dfcdddbedafde
Anthony Skelton <anthony@redcartstudios.com>
2016-01-16 05:01:33 -0800

    Updated default controller and deleted excess files

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'pages';
+$route['default_controller'] = 'slips';
 $route['slips/create'] = 'slips/create';
 // $route['slips/view/(:any)'] = 'slips/view/$1';
 $route['slips/edit/(:num)'] = 'slips/edit/$1';
