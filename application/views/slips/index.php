21abad901a51682b5e7407b179d13cb101aa40dd
Anthony Skelton <anthony@redcartstudios.com>
2016-01-17 07:45:27 -0800

    Removed table, replaced with link to Active and Archive pages

--- application/views/slips/index.php
+++ application/views/slips/index.php
@@ -6,34 +6,6 @@ if($this->session->flashdata('msg') != '' ):
 endif; 
 ?>
 
-<h2>Active Packing Slips</h2>
-<table class="table table-striped">
-	<tr>
-		<th>ID</th>
-		<th>Description</th>
-		<th>Shipped To</th>
-		<th>FedEx Tracking</th>
-		<th>Station Contact</th>
-		<th>Date Created</th>
-		<th>Last Modified</th>
-		<th>Status</th>
-	</tr>
-<?php 
-$reverseSlips = array_reverse($slips);
-foreach ($reverseSlips as $slips_item): 
-	if ($slips_item['slip_status'] != 'Complete'){
-		$url = $slips_item['slip_fedexTracking'];
-
-	echo "<tr><td><a href='" . site_url('slips') . "/" . $slips_item['slip_id'] . "'>" . $slips_item['slip_id'] .
-                            "</a></td><td><a href='" . site_url('slips') . "/" . $slips_item['slip_id'] . "'>" . $slips_item['slip_description'] . 
-                            "</a></td><td>" . $slips_item['slip_shipName'] . 
-                            "</td><td><a target='_blank' href='https://www.fedex.com/apps/fedextrack/?action=track&language=english&cntry_code=us&tracknumbers=" . $slips_item['slip_fedexTracking'] . "'>" . $slips_item['slip_fedexTracking'] . "</a>" .
-                            "</td><td>" . $slips_item['slip_customerContact'] . 
-                            "</td><td>" . date( 'F j, Y g:i a', strtotime($slips_item['slip_date'])) . 
-                            "</td><td>" . $slips_item['slip_lastModified'] .
-                            "</td><td>" . $slips_item['slip_status'] . 
-                            "</td><tr>"; 
- }
-endforeach ?>
-
-</table>
+<h2>Packing Slips</h2>
+	<button href="<?php echo site_url('active/index'); ?>" class="btn btn-lg btn-primary btn-block">Active Slips</button>
+	<button href="<?php echo site_url('slips/archive'); ?>" class="btn btn-lg btn-success btn-block">Archive Slips</button>
