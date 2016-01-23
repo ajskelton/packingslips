dd01eb28ff0e7f63ba4310e41f85511f260b1c04
Anthony Skelton <anthony@redcartstudios.com>
2016-01-23 08:52:16 -0800

    Added viewport to html head

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>KCRA Packing Slips</title>
 	<link rel="stylesheet" type="text/css" media="screen, projection" href="<?php echo asset_url(); ?>css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" media="screen, projection" href="<?php echo asset_url(); ?>css/style.css">
