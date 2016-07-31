512915b4b4b656a1cf3fd8aa83ddfc66f5d953a7
Anthony Skelton <anthony@redcartstudios.com>
2016-07-31 07:57:41 -0700

    Added slip table structure to migration file

--- application/database/migrations/20160731164925_slips.php
+++ application/database/migrations/20160731164925_slips.php
@@ -3,14 +3,83 @@
 class Migration_slips extends CI_Migration {
 
     public function up() {
-        $this->dbforge->add_field(array(
-            'id' => array(
-                'type' => 'INT',
-                'constraint' => 11,
-                'auto_increment' => TRUE
+        $this->dbforge->add_field(
+            array(
+                'slip_id' => array(
+                    'type' => 'INT',
+                    'constraint' => 11,
+                    'auto_increment' => true,
+                    'null' => false,
+                ),
+                'slip_shipName' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 50,
+                    'null' => false,
+                ),
+                'slip_shipAddress' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => false,
+                ),  
+                'slip_shipCity' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => false,
+                ),
+                'slip_shipState' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => false,
+                ),
+                'slip_shipZip' => array(
+                    'type' => 'INT',
+                    'constraint' => 5,
+                    'null' => false,
+                ),
+                'slip_fedexTracking' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => true,
+                ),
+                'slip_rmaNumber' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => true,
+                ),
+                'slip_comments' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => true,
+                ),
+                'slip_status' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => false,
+                ),
+                'slip_date timestamp default current_timestamp',
+                'slip_customerContact' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 50,
+                    'null' => false,
+                ),
+                'slip_customerPhone' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 50,
+                    'null' => false,
+                ),
+                'slip_lastModified' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 50,
+                    'null' => false,
+                ),
+                'slip_description' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 100,
+                    'null' => false,
+                ),
             )
-        ));
-        $this->dbforge->add_key('id', TRUE);
+        );
+        $this->dbforge->add_key('slip_id', TRUE);
         $this->dbforge->create_table('slips');
     }
 
